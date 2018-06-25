65c3d0a1f6eff0bfe2ee7a14a08cc3ff4300d0a8
lakshmanbhupathi <lakshmanbhupathi@gmail.com>
2018-06-26 01:14:30 +0530

    master: Check-in application structure

---
+++ zoomcar/src/test/java/com/lakshman/sample/zoomcar/ZoomcarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lakshman.sample.zoomcar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZoomcarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
